6bf15ec62fb48225e8b0e33031185a4591f55bc3
Rasi K P <rasir239@gmail.com>
2024-08-06 00:53:01 +0530

    last

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const {nextui} = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
